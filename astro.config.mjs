c1c08279c50aa36b945048eac343b62966da01be
Lucas Miranda <lucamiranda.png@gmail.com>
2025-04-25 01:31:53 -0300

    Correção de link de acesso para o link do vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://lucasmirandadev.com',
+  site: 'https://lucasmirandadev.vercel.app/',
   integrations: [tailwind(), react()],
 });
